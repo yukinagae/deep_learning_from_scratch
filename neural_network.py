def36d0aa42150d452240670898c49e362ab56d1
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-06-13 18:13:37 +0900

    forward

--- neural_network.py
+++ neural_network.py
@@ -22,6 +22,12 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
+# 活性化関数
+#
+# 恒等関数
+def identity_function(x):
+    return x
+
 # demo
 x = np.arange(-5.0, 5.0, 0.1)
 y = sigmoid(x)
@@ -29,10 +35,30 @@ plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
-X = np.array([1, 2])
-X.shape
-W = np.array([[1, 3, 5], [2, 4, 6]])
-W.shape
-Y = np.dot(X, W)
-Y.shape
-Y
+# networkの初期化
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+# network内のforward処理
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+y
