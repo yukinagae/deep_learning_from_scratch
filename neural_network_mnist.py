469eb2f4080f12886183e7978d449de9a6cb7f57
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-06-17 15:57:57 +1000

    batch = 10

--- neural_network_mnist.py
+++ neural_network_mnist.py
@@ -31,7 +31,17 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+batch_size = 100
 accuracy_cnt = 0
+
+# batch
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+# not batch
 for i in range(len(x)):
     y = predict(network, x[i])
     p = np.argmax(y)
